ce82402a71156f2226ed5fce25e19d830a83417a
Andrei George <80982595+aandreigeorge@users.noreply.github.com>
2024-01-30 16:24:37 +0200

    Update Main.java
    Refactored and removed the unnecessary name parameter that was being passed to the subtractPencilsAndCheckWinner. Winner name could be determined using the isHuman boolean value.

--- Main.java
+++ Main.java
@@ -2,32 +2,39 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         playGame();
         scanner.close();
     }
+
     public static void playGame() {
+
         System.out.println("How many pencils would you like to use:");
         int pencilsOnTable = takePencilInput();
+
         System.out.println("Who will be the first (John, Jack):");
         boolean humansTurn = takePlayerName().equalsIgnoreCase("John");
         String playerName = "John";
         String botName = "Jack";
+
         printPencils(pencilsOnTable);
 
         while(pencilsOnTable > 0) {
             if(humansTurn) {
                 System.out.println(playerName + "'s turn");
-                pencilsOnTable = subtractPencilsAndCheckWinner(pencilsOnTable, playerName, humansTurn);
+                pencilsOnTable = subtractPencilsAndCheckWinner(pencilsOnTable, humansTurn);
             } else {
                 System.out.println(botName + "'s turn");
-                pencilsOnTable = subtractPencilsAndCheckWinner(pencilsOnTable, botName, humansTurn);
+                pencilsOnTable = subtractPencilsAndCheckWinner(pencilsOnTable, humansTurn);
             }
             humansTurn = !humansTurn;
         }
     }
+
     public static int takePencilInput() {
 
         String startingPencils = scanner.nextLine();
@@ -45,8 +52,11 @@ public class Main {
         }
         return pencilsOnTable;
     }
+
     public static String takePlayerName(){
+
         String playerName = scanner.nextLine();
+
         if(playerName.equalsIgnoreCase("John") || playerName.equalsIgnoreCase("Jack")) {
             return playerName;
         } else {
@@ -54,29 +64,33 @@ public class Main {
             return takePlayerName();
         }
     }
+
     public static void printPencils(int pencils) {
+
         String pencil = "|";
-        System.out.println(pencil.repeat(pencils));
+        if(pencils != 0) {
+            System.out.println(pencil.repeat(pencils));
+        }
     }
-    public static int subtractPencilsAndCheckWinner (int pencilsOnTable, String name, boolean isHuman) {
+
+    public static int subtractPencilsAndCheckWinner (int pencilsOnTable, boolean isHuman) {
+
         int pencilsTaken;
 
         if(isHuman) {
-
             try{
                 pencilsTaken = Integer.parseInt(scanner.nextLine());
 
                 if(pencilsTaken < 1 || pencilsTaken > 3) {
                     System.out.println("Possible values: '1', '2', '3'");
-                    return subtractPencilsAndCheckWinner(pencilsOnTable, name, true);
+                    return subtractPencilsAndCheckWinner(pencilsOnTable, true);
                 } else if(pencilsTaken > pencilsOnTable) {
                     System.out.println("Too many pencils were taken");
-                    return subtractPencilsAndCheckWinner(pencilsOnTable, name, true);
+                    return subtractPencilsAndCheckWinner(pencilsOnTable,true);
                 }
-
             } catch (NumberFormatException e) {
                 System.out.println("Possible values: '1', '2' or '3'");
-                return subtractPencilsAndCheckWinner(pencilsOnTable, name, true);
+                return subtractPencilsAndCheckWinner(pencilsOnTable, true);
             }
 
         } else {
@@ -103,7 +117,7 @@ public class Main {
         printPencils(pencilsOnTable);
 
         if(pencilsOnTable  == 0) {
-            System.out.println((name.equalsIgnoreCase("John") ? "Jack" : "John") + " won!");
+            System.out.println((isHuman ? "Jack" : "John") + " won");
             return 0;
         }
         return pencilsOnTable;
